50d7de20abe0b882da9b4dff5ed34abf941d2852
nyapblesso-a11y <nyapblesso@gmail.com>
2026-02-23 12:15:51 +0100

    the search fucntionality has been implemented

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
-const BASE_URL = "http://localhost:4040"
-
+const BASE_URL = "http://localhost:4040";
 
 export async function fetchProducts() {
-  const res = await fetch(`${BASE_URL}/product`)
-  return res.json()
+  const res = await fetch(`${BASE_URL}/product`);
+  return res.json();
 }
 
 export async function createProduct(data) {
   const res = await fetch(`${BASE_URL}/product`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
+  return res.json();
+}
+
+export async function searchProducts(search, category) {
+  let query = `${BASE_URL}/product?`;
+
+  if (search) query += `search=${encodeURIComponent(search)}&`;
+  if (category) query += `category=${encodeURIComponent(category)}`;
+
+  const res = await fetch(query);
   return res.json();
 }
